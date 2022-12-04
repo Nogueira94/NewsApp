510b5fde40ac09a73c27be9b8c92ea50999eaae4
Guilherme Nogueira <guilherme.nogueira@outlook.com>
2022-12-04 00:00:11 +0000

    Fix binding main activity

--- app/src/main/java/com/appnews/presenter/MainActivity.kt
+++ app/src/main/java/com/appnews/presenter/MainActivity.kt
@@ -3,10 +3,15 @@ package com.appnews.presenter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.appnews.R
+import com.appnews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
